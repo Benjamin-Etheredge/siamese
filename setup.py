124111af2ea3958b7813452b7bc1e59e4c9499c2
ben <benjamin.etheredge@gmail.com>
2021-02-02 17:45:01 -0600

    Upping version.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="siamese-bde",
-    version="0.0.2",
+    version="0.0.3",
     author="Benjamin Etheredge",
     author_email="",
     description="A small siamese network package",
