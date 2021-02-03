da3d64624ae84c1c36e43fbbdde0d4aeabdfe4c4
ben <benjamin.etheredge@gmail.com>
2021-02-03 10:27:17 -0600

    Fixing spacing

--- bump_patch.sh
+++ bump_patch.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-old_version= $(grep version setup.py | sed -E 's/.*version.*=.*"([[:digit:]]+\.[[:digit:]]\.[[:digit:]]+)".*/\1/')
+old_version=$(grep version setup.py | sed -E 's/.*version.*=.*"([[:digit:]]+\.[[:digit:]]\.[[:digit:]]+)".*/\1/')
 bumpversion --current-version $old_version patch setup.py
-new_version= $(grep version setup.py | sed -E 's/.*version.*=.*"([[:digit:]]+\.[[:digit:]]\.[[:digit:]]+)".*/\1/')
+new_version=$(grep version setup.py | sed -E 's/.*version.*=.*"([[:digit:]]+\.[[:digit:]]\.[[:digit:]]+)".*/\1/')
 echo "::set-output name=some_output::$new_version"
